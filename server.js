b8fc894f699759947bb5dff596554875edfe05fd
Rugwed Bodhankar <rugwed07@gmail.com>
2023-10-27 14:04:19 +0530

    connecting to cloudmongodb

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://127.0.0.1/netflix", {
+  .connect("mongodb+srv://admin:1234@cluster0.vbxampy.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
